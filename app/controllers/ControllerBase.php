13159ac1b464f12fd399da326ccaf0e7ce460830
Lee Watson <rev@revthefox.co.uk>
2015-08-05 19:22:16 +0100

    Update title to use app config

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -6,7 +6,7 @@ class ControllerBase extends Controller
 {
     protected function initialize()
     {
-        $this->tag->prependTitle('FoxPaste - ');
+        $this->tag->prependTitle($this->conf->app->name . ' | ');
         $this->view->setTemplateAfter('main');
     }
 }
