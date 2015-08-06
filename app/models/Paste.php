3d9b50dc42701573f6d4ac8c92b0c9753c58d30b
Lee Watson <rev@revthefox.co.uk>
2015-08-06 21:49:55 +0100

    Added initial migration class for future migrations

---
+++ app/models/Paste.php
@@ -0,0 +1,94 @@
+<?php
+
+use Phalcon\Mvc\Model\Validator\Uniqueness;
+
+class Paste extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $slug;
+
+    /**
+     *
+     * @var string
+     */
+    public $content;
+
+    /**
+     *
+     * @var string
+     */
+    public $language;
+
+    /**
+     *
+     * @var integer
+     */
+    public $private;
+
+    /**
+     *
+     * @var string
+     */
+    public $creator_ipv4;
+
+    /**
+     *
+     * @var string
+     */
+    public $created;
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'pastes';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Paste[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Paste
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    public function validation()
+    {
+
+        $this->validate(new Uniqueness(
+            array(
+                "field"   => "slug",
+                "message" => "Paste ID must be unique!"
+            )
+        ));
+
+        return $this->validationHasFailed() != true;
+    }
+}
