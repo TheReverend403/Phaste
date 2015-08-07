236402da2a29efd05334f15d8938a1b287990c68
Lee Watson <rev@revthefox.co.uk>
2015-08-07 08:01:05 +0100

    New migration

---
+++ app/migrations/1.0.1/pastes.php
@@ -0,0 +1,94 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class PastesMigration_101 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'pastes',
+            array(
+            'columns' => array(
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'slug',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 13,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'content',
+                    array(
+                        'type' => Column::TYPE_TEXT,
+                        'size' => 1,
+                        'after' => 'slug'
+                    )
+                ),
+                new Column(
+                    'language',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 10,
+                        'after' => 'content'
+                    )
+                ),
+                new Column(
+                    'private',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'language'
+                    )
+                ),
+                new Column(
+                    'creator_ipv4',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 64,
+                        'after' => 'private'
+                    )
+                ),
+                new Column(
+                    'created',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'creator_ipv4'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id')),
+                new Index('slug', array('slug'))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '576',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_general_ci'
+            )
+        )
+        );
+    }
+}
