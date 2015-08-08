4c5e9fd98fb110154863aad1c6940b723765c641
Lee Watson <rev@revthefox.co.uk>
2015-08-09 00:39:51 +0100

    Update routes to match generated slugs

--- app/config/routes.php
+++ app/config/routes.php
@@ -20,19 +20,19 @@ $router->addPost(
 
 // Covers /v/ and /view/
 $router->addGet(
-    "/v(iew)?/([a-zA-Z0-9]{1,13})",
+    "/v(iew)?/([a-zA-Z0-9]{5,13})",
     array(
         "controller" => "view",
-        "id"     => 2 // ([a-zA-Z0-9]{1,13})
+        "id"     => 2 // ([a-zA-Z0-9]{5,13})
     )
 );
 
 $router->addGet(
-    "/v(iew)?/([a-zA-Z0-9]{1,13})/raw",
+    "/v(iew)?/([a-zA-Z0-9]{5,13})/raw",
     array(
         "controller" => "view",
         "action" => "raw",
-        "id"     => 2 // ([a-zA-Z0-9]{1,13})
+        "id"     => 2 // ([a-zA-Z0-9]{5,13})
     )
 );
 
