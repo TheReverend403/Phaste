96ee4e6393eedb10ec68472dae08faee4d0db729
Lee Watson <rev@revthefox.co.uk>
2015-08-09 19:04:56 +0100

    Added configurable timezone

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -4,8 +4,12 @@ use Phalcon\Mvc\Controller;
 
 class ControllerBase extends Controller
 {
-    protected function initialize()
-    {
-        $this->tag->setTitle($this->config->app->name . ' | ');
-    }
+	protected function initialize()
+	{
+		if ($this->config->app->timezone != 'system')
+		{
+			date_default_timezone_set($this->config->app->timezone);
+		}
+		$this->tag->setTitle($this->config->app->name . ' | ');
+	}
 }
