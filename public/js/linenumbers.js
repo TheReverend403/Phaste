27e184ecf80053c949cfdee949d7cf0475c0641a
GunfighterJ <joseph.jenniges@gmail.com>
2015-08-19 14:03:29 -0500

    Add clickable lines

--- public/js/linenumbers.js
+++ public/js/linenumbers.js
@@ -1,13 +1,31 @@
-// https://jsfiddle.net/tovic/AbpRD/
 (function() {
-    var pre = document.getElementsByTagName('pre'),
-        pl = pre.length;
-    for (var i = 0; i < pl; i++) {
-        pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
-        var num = pre[i].innerHTML.split(/\n/).length;
-        for (var j = 0; j < num; j++) {
-            var line_num = pre[i].getElementsByTagName('span')[0];
-            line_num.innerHTML += '<span>' + (j + 1) + '</span>';
-        }
+    // Get the only pre tag on the page
+    var pre = document.getElementsByTagName('pre')[0];
+    
+    // Create span element, which will be our line numbers
+    var span = document.createElement('span');
+    span.className = "line-number";
+    
+    // Get number of lines in paste
+    var numberOfLines = pre.innerHTML.split(/\n/).length;
+    
+    // Iterate each line
+    for (i = 0, j = 1; i < numberOfLines; i++, j++)
+    {
+        // Create a new child span, set its attributes accordingly
+        var lineSpan = document.createElement("span");
+        lineSpan.id = 'L' + j;
+        lineSpan.setAttribute("onClick", "document.location.hash = this.id")
+        lineSpan.innerHTML = j;
+        
+        // Add to line-numbers span
+        span.appendChild(lineSpan);
     }
+    
+    // Insert before the rest of the code
+    pre.insertBefore(span, pre.firstChild);
+
+    cl = document.createElement("span");
+    cl.className = "cl";
+    pre.appendChild(cl);
 })();
